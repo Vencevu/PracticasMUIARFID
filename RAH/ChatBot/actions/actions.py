ca85ed73497bb94536a6f3b4cac7211986b874d4
Vencevu <albertovencevu@gmail.com>
2022-12-21 21:17:01 +0100

    ok

---
+++ RAH/ChatBot/actions/actions.py
@@ -0,0 +1,34 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionBuyProduct(Action):
+
+    def name(self) -> Text:
+        return "buy_product"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        p = next(tracker.get_latest_entity_values("producto"), None)
+
+        if not p:
+            msg = "Producto no encontrado"
+        else:
+            msg = str(p)+" encargado"
+
+        dispatcher.utter_message(text=msg)
+
+        return []
