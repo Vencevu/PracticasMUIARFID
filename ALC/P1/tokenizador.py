9a0bd7c6cdda32dd002048545f5076e30ab3c305
Vencevu <albertovencevu@gmail.com>
2023-03-23 22:05:06 +0100

    ok

--- ALC/P1/tokenizador.py
+++ ALC/P1/tokenizador.py
@@ -2,9 +2,9 @@ import re
 import argparse
 
 specialDatePattern = re.compile(
-    "[0-31] de (?:enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre) de \d{4,}",
-    re.I | re.U)
-acroPattern = re.compile("(?:EE.UU.|S.L.|CC.OO.|S.A.|D.|U.R.S.S.)")
+    "([0-31] de (enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)( de \d{4,})*)",
+)
+acroPattern = re.compile("(EE.UU.|S.L.|CC.OO.|S.A.|D.|U.R.S.S.)")
 pattern = re.compile(
     r"\b\S+\b|[(),\'\"?¿!¡:;%]|\.+|^\d*[.,]?\d*|\S+@\S+|\d{1,2}:\d{2}[h]?|http[s]?://[w{3}.]?\S*|\d{1,2}(?:|-)\d{2}(?:|-)\d{2,4}|@\S+|#\S+")
 
@@ -14,11 +14,14 @@ def tokenizarDocumento(listOfSentences):
     for sentence in listOfSentences:
         result += "%s\n" % sentence
         dates = specialDatePattern.findall(sentence)
+        dates = [d[0] for d in dates]
+        print("DATES: ", dates)
         sentenceClean = re.sub(specialDatePattern, "DATE", sentence)
         acros = acroPattern.findall(sentenceClean)
+        print("ACROS: ", acros)
         sentenceClean = re.sub(acroPattern, "ACRO", sentenceClean)
         for token in pattern.findall(sentenceClean):
-            print(token)
+            # print(token)
             if token == "ACRO":
                 acro = acros.pop(0)
                 result += "%s \n" % acro
@@ -36,8 +39,8 @@ def tokenizarDocumento(listOfSentences):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Tokenizador de español')
-    parser.add_argument("--inputFile", dest="inputFile", type=str)
-    parser.add_argument("--outputFile", dest="outputFile", type=str)
+    parser.add_argument("--i", dest="inputFile", type=str)
+    parser.add_argument("--o", dest="outputFile", type=str)
     args = parser.parse_args()
     inputSentences = open(args.inputFile, "r", encoding="utf-8").readlines()
     result = tokenizarDocumento(inputSentences)
