31f9f84ffa4ab6d34e90c858370230ba9c0642a8
Vencevu <albertovencevu@gmail.com>
2023-05-17 20:37:24 +0200

    ok

--- BIO/FisherFaces/fisherfaces.py
+++ BIO/FisherFaces/fisherfaces.py
@@ -1,34 +1,30 @@
 import sys
 import os 
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np  
 from numpy import linalg as LA              
-import matplotlib.pyplot as plt   
 from sklearn.neighbors import KNeighborsClassifier
 
 def read_pgm(archivo):
     with open(archivo) as f:
         lines = f.readlines()
 
-    for l in list(lines):
-        if l[0] == '#':
-            lines.remove(l)
-
+    lines = [l for l in lines if not l.startswith('#')]
     assert lines[0].strip() == 'P2' 
 
     data = []
     for line in lines[1:]:
         data.extend([int(c) for c in line.split()])
 
-    return (np.array(data[3:]),(data[1],data[0]),data[2])
+    return np.array(data[3:]), (data[1], data[0]), data[2]
 
-def procesarDatos():
+def procesar_datos():
     x_train = []
     y_train = []
     x_test = []
     y_test = []
     Nc = []
-    for ruta, _ , archivos in os.walk("../data/IDENTIFICACION/ORLProcessed/Train/", topdown=False):
+    for ruta, _, archivos in os.walk("../data/IDENTIFICACION/ORLProcessed/Train/", topdown=False):
         Nc.append(len(archivos))
         for archivo in archivos:
             etiqueta = int(ruta[-3:].split('s')[1])
@@ -36,7 +32,7 @@ def procesarDatos():
             
             imagen = read_pgm(os.path.join(ruta, archivo))
             x_train.append(imagen[0])
-    for ruta, _ , archivos in os.walk("../data/IDENTIFICACION/ORLProcessed/Test/", topdown=False):
+    for ruta, _, archivos in os.walk("../data/IDENTIFICACION/ORLProcessed/Test/", topdown=False):
         for archivo in archivos:
             etiqueta = int(ruta[-3:].split('s')[1])
             y_test.append(etiqueta)
@@ -51,18 +47,13 @@ def procesarDatos():
     return x_train, y_train, x_test, y_test, Nc
 
 def PCA(x_train, x_test, d_):
-    d = x_train.shape[0]
-    n = x_train.shape[1]
+    d, n = x_train.shape
     
     mu = np.mean(x_train, axis=1, keepdims=True)
 
-    A = np.zeros((d, n), dtype=np.float64)
-    A = np.asarray(A)
-    for i in range(n):
-        A[:,i] = x_train[:, i] - mu[:,0]
+    A = x_train - mu
 
-    C_ = np.dot(A.T, A)
-    C_ /= d
+    C_ = np.dot(A.T, A) / d
 
     delta_, B_ = LA.eig(C_)
 
@@ -71,12 +62,12 @@ def PCA(x_train, x_test, d_):
 
     ordenados = np.flip(np.argsort(delta))
     delta = delta[ordenados]
-    B = B[:,ordenados] 
+    B = B[:, ordenados] 
 
     normas = LA.norm(B, axis=0)
     B = B / normas
 
-    Bd = B[:,:d_]
+    Bd = B[:, :d_]
     Bdt = Bd.T
     x_train_red = np.dot(Bdt, (x_train - mu))
     x_test_red = np.dot(Bdt, (x_test - mu))
@@ -84,14 +75,10 @@ def PCA(x_train, x_test, d_):
     return x_train_red, x_test_red
 
 def kvecino(v, x_train, y_train, x_test, y_test):
-    vecino = KNeighborsClassifier(n_neighbors = v, metric ='euclidean', p = 2)
+    vecino = KNeighborsClassifier(n_neighbors=v, metric='euclidean', p=2)
     vecino.fit(x_train.T, y_train)
     y_pred = vecino.predict(x_test.T)
-    precision = 0
-    for i in range(len(y_pred)):
-        if y_pred[i]==y_test[i]:
-            precision += 1
-    precision = precision/len(y_pred)
+    precision = np.sum(y_pred == y_test) / len(y_pred)
     return precision
 
 v = int(sys.argv[1])
@@ -102,12 +89,10 @@ def LDA(x_training, x_test, Nc, d_):
     
     mu_c = []
     for i, nc in enumerate(Nc):
-        j=nc
-        while j>0:
-            mu_c.append(x_training[:,i*nc:(i*nc)+nc].mean(axis=1))
-            j-=1
-    mu_c = np.asarray(np.array(mu_c)).T
-    if mu_c.shape[1]==1:
+        for _ in range(nc):
+            mu_c.append(x_training[:, i*nc:(i*nc)+nc].mean(axis=1))
+    mu_c = np.asarray(mu_c).T
+    if mu_c.shape[1] == 1:
         mu_c = mu_c.T
 
     Sb = np.mat(mu_c) - np.mat(mu).T
@@ -117,24 +102,24 @@ def LDA(x_training, x_test, Nc, d_):
     
     Sw = x_training - mu_c
     Sw = np.mat(Sw) * np.mat(Sw).T
-    C = LA.inv(Sw)*Sb
+    C = LA.inv(Sw) * Sb
 
     delta, B = LA.eigh(C)
 
     ordenados = np.flip(np.argsort(delta))
     delta = delta[ordenados]
-    B = B[:,ordenados] 
+    B = B[:, ordenados] 
 
-    Bd = B[:,:d_]
+    Bd = B[:, :d_]
     Bdt = Bd.T
     x_training_red = Bdt * x_training
     x_test_red = Bdt * x_test
 
     return x_training_red, x_test_red
 
-x_training, y_training, x_test, y_test, Nc = procesarDatos()
+x_training, y_training, x_test, y_test, Nc = procesar_datos()
 precisiones = []
-reduccionesLDA = [i for i in np.arange(5, 205, 5)]
+reduccionesLDA = np.arange(5, 205, 5)
 for d_lda in reduccionesLDA:
     x_training_pca, x_test_pca = PCA(x_training, x_test, d_pca)
     x_training_lda, x_test_lda = LDA(x_training_pca, x_test_pca, Nc, d_lda)
@@ -144,11 +129,11 @@ for d_lda in reduccionesLDA:
 max_precision = max(precisiones)
 indice = precisiones.index(max_precision)
 mejor_reduccion = reduccionesLDA[indice]
-print("Con una reducción previa de PCA de %d, la mejor reducción con LDA es %d, obteniendo una precision de %f" % (d_pca, mejor_reduccion, max_precision))
-title = "Reducción PCA+LDA y Clasificación con "+str(v)+"-NN"
+print(f"Con una reducción previa de PCA de {d_pca}, la mejor reducción con LDA es {mejor_reduccion}, obteniendo una precisión de {max_precision}")
+title = f"Reducción PCA+LDA y Clasificación con {v}-NN"
 plt.title(title)
 plt.plot(reduccionesLDA, precisiones)
-plt.xlabel("Valor de d' para LDA con una reducción previa de %d con PCA" % d_pca)
+plt.xlabel(f"Valor de d' para LDA con una reducción previa de {d_pca} con PCA")
 plt.ylabel("Precisión (%)")
-nombre = "Resultados_PCA_LDA_"+str(v)+"nn.png"
+nombre = f"Resultados_PCA_LDA_{v}nn.png"
 plt.savefig(nombre)
